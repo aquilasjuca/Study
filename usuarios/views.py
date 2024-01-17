22137418ae61c82d12877280637a49c55a263053
Áquilas Rodrigues Jucá <aquilasjuca2016@gmail.com>
2024-01-17 13:52:51 -0300

    Changing the app name

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def register(request):
-    return render(request, 'register.html')
+def cadastro(request):
+    return render(request, 'cadastro.html')
